80a9ae68f8853959cd56c6ee882a9b8ef74b7800
Sebastiano Licciardello <80095600+sebastianolicciardello@users.noreply.github.com>
2025-04-15 08:23:16 +0200

    Added icons and updated name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.betullarise"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
